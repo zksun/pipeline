739fada10f29921ae97993c5cd0f629f44c8c779
缉熙 <zhikun.szk@alibaba-inc.com>
2017-06-08 12:43:53 +0800

    big update

--- common/src/main/java/com/sun/pipeline/util/calculate/Factor.java
+++ common/src/main/java/com/sun/pipeline/util/calculate/Factor.java
@@ -4,11 +4,37 @@ package com.sun.pipeline.util.calculate;
  * Created by zksun on 08/06/2017.
  */
 public interface Factor<R> {
+    /**
+     * 算子的唯一id号
+     *
+     * @return long型的唯一号
+     */
     long id();
 
+    /**
+     * 算子的当前值
+     *
+     * @return
+     */
     R value();
 
-    default Factor<R> merge(Factor f) {
-        return this.merge(f);
-    }
+    /**
+     * @param factor
+     * @return
+     */
+    Factor<R> merge(Factor factor);
+
+    /**
+     * 算子中是不是有值
+     * @return
+     */
+    boolean hasValue();
+
+    /**
+     * 算子的当前状态
+     * @return
+     */
+    int status();
+
+
 }
