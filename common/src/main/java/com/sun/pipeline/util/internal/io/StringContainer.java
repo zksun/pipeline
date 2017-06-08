a6d520edb240fc80feeafc9bdd2992e787caabd5
缉熙 <zhikun.szk@alibaba-inc.com>
2017-06-08 20:41:39 +0800

    big update

---
+++ common/src/main/java/com/sun/pipeline/util/internal/io/StringContainer.java
@@ -0,0 +1,15 @@
+package com.sun.pipeline.util.internal.io;
+
+/**
+ * Created by zksun on 08/06/2017.
+ */
+public abstract class StringContainer<D> extends ContainerAdapter<D, Object> {
+
+    @Override
+    public Container swallow(String s) {
+        add(convert(s));
+        return this;
+    }
+
+    abstract D convert(String s);
+}
