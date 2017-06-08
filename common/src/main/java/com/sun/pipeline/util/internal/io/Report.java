c11b086b7ec22a91b0fe79947ddc0fc290c58749
缉熙 <zhikun.szk@alibaba-inc.com>
2017-06-08 14:54:02 +0800

    big update

--- common/src/main/java/com/sun/pipeline/util/internal/io/Report.java
+++ common/src/main/java/com/sun/pipeline/util/internal/io/Report.java
@@ -3,6 +3,6 @@ package com.sun.pipeline.util.internal.io;
 /**
  * Created by zksun on 08/06/2017.
  */
-public interface Report<R> {
-    R report();
+public interface Report {
+    void report();
 }
