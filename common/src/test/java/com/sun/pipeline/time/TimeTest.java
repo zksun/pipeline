f681b4fbcf8e1de0c844085f9e4f9be31aad589b
缉熙 <zhikun.szk@alibaba-inc.com>
2017-06-13 16:47:43 +0800

    big update

--- common/src/test/java/com/sun/pipeline/time/TimeTest.java
+++ common/src/test/java/com/sun/pipeline/time/TimeTest.java
@@ -3,6 +3,7 @@ package com.sun.pipeline.time;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by zksun on 13/06/2017.
@@ -10,8 +11,14 @@ import java.time.LocalDateTime;
 public class TimeTest {
 
     @Test
-    public void timeMinuteSecondTest() {
-        LocalDateTime localDateTime = LocalDateTime.of(2015, 4, 23, 9, 1, 0);
+    public void timeYearDayMinuteSecondTest() {
+        LocalDateTime localDateTime = LocalDateTime.of(2015, 4, 23, 9, 1);
         System.out.println(localDateTime);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime parse = LocalDateTime.parse("2015/12/26 09:00:00", formatter);
+        System.out.println(parse);
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        System.out.println(formatter1.format(localDateTime));
     }
+
 }
