a8254f773506db27d44f157607f2944b10a1df5c
缉熙 <zhikun.szk@alibaba-inc.com>
2017-09-21 21:21:48 +0800

    reflect

---
+++ stock/src/test/java/com/sun/pipeline/stock/domain/ExcludeRightsWrapperTest.java
@@ -0,0 +1,26 @@
+package com.sun.pipeline.stock.domain;
+
+import com.sun.pipeline.stock.explorer.sohu.SohuStockHttpCommandService;
+import com.sun.pipeline.util.internal.http.HttpGet;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Created by zksun on 2017/9/21.
+ */
+public class ExcludeRightsWrapperTest {
+
+    @Test
+    public void excludeRightsTest() {
+        SohuStockHttpCommandService instance = SohuStockHttpCommandService.getInstance();
+        HttpGet httpGetInstance = HttpGet.getHttpGetInstance("http://q.stock.sohu.com", 80, "/qp/hq", 3);
+        List<ExcludeRights> sz_600030 = instance.getExcludeRightsInfo(httpGetInstance, "sz_600030");
+
+        ExcludeRightsWrapper rightsWrapper = ExcludeRightsWrapper.getInstance(sz_600030);
+        long l = rightsWrapper.calculateAdjustStockPrice(LocalDate.of(2017, 9, 20), 1800);
+        System.out.println(l);
+    }
+
+}
