65d394bc2fdb49d58982ae3399700ff5aad29af0
coderzksun@gmail.com <coderzksun@gmail.com>
2017-11-07 11:40:07 +0800

    spring dependency

--- stock/src/main/java/com/sun/pipeline/stock/Contants.java
+++ stock/src/main/java/com/sun/pipeline/stock/Contants.java
@@ -1,11 +1,8 @@
 package com.sun.pipeline.stock;
 
-import com.sun.pipeline.handler.info.InformationHandler;
-import com.sun.pipeline.stock.domain.KlineItem;
 import com.sun.pipeline.stock.infomation.sohu.SohuForwardAnswerAuthorityInformationHandler;
 import com.sun.pipeline.stock.infomation.sohu.SohuKlineInformationHandler;
 import com.sun.pipeline.util.internal.http.HttpGet;
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 /**
  * Created by zksun on 13/06/2017.
