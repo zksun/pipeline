98f680a1ba41738f16b0ec09f8e21b3dd8274238
coderzksun@gmail.com <coderzksun@gmail.com>
2017-11-12 15:11:15 +0800

    spring dependency

--- stock/src/main/java/com/sun/pipeline/stock/command/BaseDayCommand.java
+++ stock/src/main/java/com/sun/pipeline/stock/command/BaseDayCommand.java
@@ -1,17 +1,24 @@
 package com.sun.pipeline.stock.command;
 
+import com.sun.pipeline.mybatis.domain.StockBaseDO;
 import com.sun.pipeline.stock.Command;
+import com.sun.pipeline.stock.domain.StockPrice;
+import com.sun.pipeline.stock.price.StockDayContainer;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zhikunsun on 17/9/24.
  */
 public class BaseDayCommand implements Command {
 
-    private LocalDate day;
+    private final StockDayContainer stockDayContainer;
 
-    private String stockCode;
+    private BaseDayCommand(StockDayContainer container) {
+        this.stockDayContainer = container;
+    }
 
     @Override
     public int commandId() {
@@ -25,9 +32,15 @@ public class BaseDayCommand implements Command {
 
     @Override
     public boolean doCommand() throws Exception {
+        List<StockPrice> data = stockDayContainer.getData();
+        if (null != data && !data.isEmpty()) {
+            List<StockBaseDO> stockBaseDOs = new ArrayList<>();
+            for (StockPrice price : data) {
+
+            }
+        }
         return false;
     }
-
 
 
 }
