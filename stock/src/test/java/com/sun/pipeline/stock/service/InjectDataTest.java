727de8e4ad8eb5453f342bdd7515897c0e279efa
coderzksun@gmail.com <coderzksun@gmail.com>
2017-11-12 10:52:07 +0800

    spring dependency

--- stock/src/test/java/com/sun/pipeline/stock/service/InjectDataTest.java
+++ stock/src/test/java/com/sun/pipeline/stock/service/InjectDataTest.java
@@ -1,14 +1,22 @@
 package com.sun.pipeline.stock.service;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import javax.annotation.Resource;
 
 /**
  * Created by zhikunsun on 2017/11/12.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class InjectDataTest {
     @Resource
     private InjectDataService injectDataService;
 
+    @Test
     public void injectStockCodeTest() {
         boolean b = injectDataService.injectAllStockCode();
         System.out.println(b);
