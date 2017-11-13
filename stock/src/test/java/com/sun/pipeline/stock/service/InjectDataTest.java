2ea276dba4a65a6cd581db2a0a260c19560572a5
coderzksun@gmail.com <coderzksun@gmail.com>
2017-11-13 09:59:43 +0800

    spring dependency

--- stock/src/test/java/com/sun/pipeline/stock/service/InjectDataTest.java
+++ stock/src/test/java/com/sun/pipeline/stock/service/InjectDataTest.java
@@ -1,7 +1,5 @@
 package com.sun.pipeline.stock.service;
 
-import com.sun.pipeline.stock.StockUtil;
-import com.sun.pipeline.stock.domain.Stock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
